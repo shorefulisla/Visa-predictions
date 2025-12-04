1b008e17a76fb253b332b5e20ce4825a40ff0222
shoreful islam <abcd@gamil.com>
2025-12-04 09:03:49 +0600

    ohh shit many many commit are there

---
+++ demo.py
@@ -0,0 +1,14 @@
+from us_visa.logger import logging
+
+from us_visa.exception import USvisaException
+
+import sys
+
+try:
+    a = 1 / 0
+    print(a)
+except Exception as e:
+    raise USvisaException(e, sys) 
+
+
+logging.info("This is an info mfsdkasdfasdf sdfessage from demo.py")
